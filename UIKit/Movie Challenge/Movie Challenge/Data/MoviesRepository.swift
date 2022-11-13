65614ffdd3304ebdd7de4ff7f49ea69327d35f5c
Victor Monteiro <victor.monteiro21@icloud.com>
2022-11-12 20:57:07 -0700

    created Movies Repository

---
+++ UIKit/Movie Challenge/Movie Challenge/Data/MoviesRepository.swift
@@ -0,0 +1,33 @@
+//
+//  MoviesRepository.swift
+//  Movie Challenge
+//
+//  Created by Victor Monteiro on 11/12/22.
+//
+
+import Foundation
+import Apollo
+
+protocol MoviesRepositoryProtocol {
+    func getMovies(completion: @escaping (Result<GetMoviesQueryQuery.Data,Error>) -> Void)
+}
+
+class MoviesRepository: MoviesRepositoryProtocol {
+    func getMovies(completion: @escaping (Result<GetMoviesQueryQuery.Data, Error>) -> Void) {
+        
+        let query = GetMoviesQueryQuery()
+        Network.shared.apollo.fetch(query: query) { result in
+          switch result {
+            case .success(let graphQLResult):
+              print("Found \(graphQLResult.data?.movies?.count ?? 0) movies")
+              guard let movieData = graphQLResult.data else { return }
+              completion(.success(movieData))
+            case .failure(let error):
+              print("Error getting movies: \(error.localizedDescription)")
+              completion(.failure(error))
+          }
+        }
+        
+    }
+}
+
--- UIKit/Movie Challenge/Movie Challenge/Presentation/Movies List/View/MovieListViewController.swift
+++ UIKit/Movie Challenge/Movie Challenge/Presentation/Movies List/View/MovieListViewController.swift
@@ -41,19 +41,4 @@ class MovieListViewController: UIViewController {
             self.title = title
         }).store(in: &cancellables)
     }
-    
-    func getData() {
-        // Example for how to use the demo GetMovies query to fetch data from the server.
-        let query = GetMoviesQueryQuery()
-        Network.shared.apollo.fetch(query: query) { result in
-          switch result {
-            case .success(let graphQLResult):
-              print("Found \(graphQLResult.data?.movies?.first??.title ?? "") movies")
-
-            case .failure(let error):
-              print("Error getting movies: \(error.localizedDescription)")
-          }
-        }
-    }
-
 }
